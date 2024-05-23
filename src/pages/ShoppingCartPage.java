ce231dfa028586d9e0ea92d51529779742328583
Andreea Ghita <ghita.andreea96@gmail.com>
2024-05-23 16:38:03 +0300

    sc det

--- src/pages/ShoppingCartPage.java
+++ src/pages/ShoppingCartPage.java
@@ -8,6 +8,7 @@ public class ShoppingCartPage {
     public ShoppingCartPage(WebDriver driver){
         this.driver = driver;
     }
+    private By shoppingCartButton = By.className("shopping_cart_link");
     private By checkoutButton = By.id("checkout");
     private By firstNameInput = By.id("first-name");
     private By lastNameInput = By.id("last-name");
@@ -16,6 +17,9 @@ public class ShoppingCartPage {
     private By finishButton = By.id("finish");
     private By backHomeButton = By.id("back-to-products");
 
+    public void clickShoppingCartButton() {
+        driver.findElement(shoppingCartButton).click();
+    }
     public void checkoutOrder(){
         driver.findElement(checkoutButton).click();
     }
@@ -36,5 +40,18 @@ public class ShoppingCartPage {
         driver.findElement(backHomeButton).click();
     }
 
+    public void addProductToShoppingCart() {
+        clickShoppingCartButton();
+    }
 
+    //1.ma loghez
+    //2. adaug un produs in cos
+    //3. dau click pe shopping cart button
+    //4. verific daca produsul a fost adaugat in cos
+    //5. dau click pe continue button
+    //6. creez o clasa de random generator pt a completa diferit valorile din complete data for order....
+    //7. verific?
+    //8. click pe finish order
+    //9. verific mesajul
+    //10. click pe back to pp
 }
